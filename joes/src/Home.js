da7617f3a468313d49fd094f25c7239348221e40
jidelson <58210030+jidelson@users.noreply.github.com>
2020-05-29 02:39:56 -0700

    got some animation working

--- joes/src/Home.js
+++ joes/src/Home.js
@@ -1,21 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {Route, Link} from 'react-router-dom';
 import * as yup from 'yup';
 import axios from 'axios';
 import Form from './Form';
+import {TweenMax, Power3} from 'gsap';
+
 
 function Home(){
 
+    let imgItem = useRef(null);
+    
+    useEffect(() => {
+        console.log(imgItem)
+        TweenMax.to(
+            imgItem,
+            .8,
+            {
+                opacity: 1,
+                y: -20,
+                ease: Power3.easeOut
+            }
+        )
+    }, [])
     
     return(
+        <div>
         <div>
         
             
         <h2>
         Welcome to the African Marketplace
         </h2>
-   
+        </div>
 
+        <div>
+            <img 
+            src='/images/Sauti.png' 
+            alt='women reading information provided by sauti' 
+            ref={element => {imgItem = element}}
+            />
+
+        </div>
         </div>
     )
 }
